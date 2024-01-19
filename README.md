28502f3b1f2c5b5a5bdbdf37b8df3f5073e9e7a8
Atharva Naik <atharvan.coder@gmail.com>
2024-01-19 15:30:35 +0530

    README.md Video

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Product Catalog
 
 This is a Next.js project that allows users to browse and buy products from different categories. It has features like displaying category wise items, add to cart, cart details, total items and their price total with GST.
-
+[!Project Video](/public/video/project-video.mp4)
 ## Installation
 
 To run this project, you need to have Node.js and npm installed on your machine. Then, clone this repository from GitHub:
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,38 @@
-"use client"
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+"use client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar/Navbar";
 import Catalog from "@/components/ProductCatalog/ProductCatalog";
-import Cart from "@/components/cart/cart"
+import Cart from "@/components/cart/cart";
 import Footer from "@/components/Footer/Footer";
 import { ShoppingCartProvider } from "@/context/ShoppingCartContext";
 import { Toaster } from "react-hot-toast";
+import { useEffect } from 'react';
 export default function Home() {
+  useEffect(() => {
+    if (typeof window !== 'undefined') { 
+    }
+  }, []);
   return (
     <main>
       <BrowserRouter>
-      <ShoppingCartProvider>
-      <Toaster
-  position="top-center"
-  reverseOrder={false}
-/>
-        <Navbar />
-        <Routes>
-          <Route  path="/" element={<><Hero /><Catalog/></>} ></Route>
-          <Route path="/cart" element={<Cart/>}></Route>
-        </Routes>
-        <Footer />
-      </ShoppingCartProvider>
+        <ShoppingCartProvider>
+          <Toaster position="top-center" reverseOrder={false} />
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Hero />
+                  <Catalog />
+                </>
+              }
+            ></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+          </Routes>
+          <Footer />
+        </ShoppingCartProvider>
       </BrowserRouter>
     </main>
   );
